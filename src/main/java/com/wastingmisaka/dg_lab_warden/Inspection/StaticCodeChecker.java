f36c23d18823bb74b4a3f0eaef690d5225196327
wastingmisaka <forelink@foxmail.com>
2024-09-22 21:38:39 +0800

    +StaticCodeInspection Thread

---
+++ src/main/java/com/wastingmisaka/dg_lab_warden/Inspection/StaticCodeChecker.java
@@ -0,0 +1,46 @@
+package com.wastingmisaka.dg_lab_warden.Inspection;
+
+import com.intellij.codeInsight.daemon.impl.DaemonCodeAnalyzerImpl;
+import com.intellij.codeInsight.daemon.impl.HighlightInfo;
+import com.intellij.lang.annotation.HighlightSeverity;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.vfs.VirtualFile;
+
+import java.util.List;
+
+import static com.wastingmisaka.dg_lab_warden.staticVar.statusVar.*;
+
+public class StaticCodeChecker extends Thread{
+    @Override
+    public void run() {
+        while(true){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            Project[] openProjects = ProjectManager.getInstance().getOpenProjects();
+            for (Project p : openProjects) {
+                Editor editor = FileEditorManager.getInstance(p).getSelectedTextEditor();
+                if (editor != null) {
+                    // 获取当前文件
+                    VirtualFile[] files = FileEditorManager.getInstance(p).getOpenFiles();
+                    if (files.length > 0) {
+                        List<HighlightInfo> highlightInfos = DaemonCodeAnalyzerImpl.getHighlights(editor.getDocument(), HighlightSeverity.ERROR, p);
+                        List<HighlightInfo> highlightInfos2 = DaemonCodeAnalyzerImpl.getHighlights(editor.getDocument(), HighlightSeverity.WARNING, p);
+                        long warnings = highlightInfos2.stream()
+                                .filter(info -> info.getSeverity() == HighlightSeverity.WARNING)
+                                .count();
+                        long errors = highlightInfos.stream()
+                                .filter(info -> info.getSeverity() == HighlightSeverity.ERROR)
+                                .count();
+                        System.out.println("warning: "+warnings+" error: "+errors);
+                    }
+                }
+            }
+        }
+    }
+}
